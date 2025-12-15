b03d19d56c8ed0560a26e49ac5c61a5617bd3116
Miko Kortelainen <mikokortelainen2003@gmail.com>
2025-12-15 13:43:59 +0000

    Consultant profile skeletons

---
+++ frontend/src/features/consultant/components/ProfessionalExperience/ProfessionalExperienceSection.tsx
@@ -0,0 +1,9 @@
+import { Box, Typography } from "@mui/material";
+
+export default function ProfessionalExperience() {
+  return (
+    <Box sx={{ p: 2, border: 1 }}>
+      <Typography>Consultant Professional Experience Section</Typography>
+    </Box>
+  );
+}
