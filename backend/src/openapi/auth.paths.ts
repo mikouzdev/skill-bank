3315766d772e6f3b47e7cecdb883455d6c62ecf2
heikki_vihemaki <heikki.vihemaki@gmail.com>
2025-12-16 09:24:03 +0200

    tools: set up openapi generation with zod and shared types

---
+++ backend/src/openapi/auth.paths.ts
@@ -0,0 +1,44 @@
+import {
+  LoginSchema,
+  AuthResponseSchema,
+  LogoutResponseSchema,
+} from "../schemas/auth.schema.js";
+
+export const authPaths = {
+  "/auth/login": {
+    post: {
+      summary: "Login user",
+      tags: ["Auth"],
+      security: [], //override auth require
+      requestBody: {
+        required: true,
+        content: {
+          "application/json": { schema: LoginSchema },
+        },
+      },
+      responses: {
+        "200": {
+          description: "Login successful",
+          content: {
+            "application/json": { schema: AuthResponseSchema },
+          },
+        },
+        401: { description: "Invalid credentials" },
+      },
+    },
+  },
+  "/auth/logout": {
+    post: {
+      summary: "Logout user",
+      tags: ["Auth"],
+      responses: {
+        "200": {
+          description: "Logout successful",
+          content: {
+            "application/json": { schema: LogoutResponseSchema },
+          },
+        },
+      },
+    },
+  },
+};
---
+++ backend/src/openapi/openapi.ts
@@ -0,0 +1,30 @@
+import { createDocument } from "zod-openapi";
+import { authPaths } from "./auth.paths.js";
+import type { OpenAPIV3_1 } from "openapi-types";
+
+export const openApiDoc: OpenAPIV3_1.Document = createDocument({
+  openapi: "3.1.0",
+  info: {
+    title: "SPankki API",
+    version: "1.0.0",
+    description: "This API is for SPankki recruiter app",
+  },
+  security: [{ bearerAuth: [] }], // if no auth required, override with : security: [] at the operation level
+  servers: [
+    { url: "http://localhost:3000", description: "Local development server" },
+  ],
+
+  components: {
+    securitySchemes: {
+      bearerAuth: {
+        type: "http",
+        scheme: "bearer",
+        bearerFormat: "JWT",
+      },
+    },
+  },
+
+  paths: {
+    ...authPaths, // add paths here
+  },
+}) as unknown as OpenAPIV3_1.Document; // workaround to have TS accept a mismatch between zod-openapi and openapi-types.
