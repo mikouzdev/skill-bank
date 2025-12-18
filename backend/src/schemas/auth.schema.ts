e408cfb4296daaeacfbcbbb94b052e553abcfa3d
Nuutti Rantanen <t66ravovg@mozmail.com>
2025-12-18 11:35:48 +0000

    Resolve "Add seed to Prisma"

--- backend/src/schemas/auth.schema.ts
+++ backend/src/schemas/auth.schema.ts
@@ -1,24 +1,27 @@
 import z from "zod";
 
-
 //REQ Schemas
-export const LoginSchema = z.object({
+export const LoginSchema = z
+  .object({
     email: z.email().meta({ example: "user@example.com" }),
-    password: z.string().min(6).meta({ example: "Password"}),
-}).meta({ id: "LoginRequest"});
+    password: z.string().min(6).meta({ example: "Password" }),
+  })
+  .meta({ id: "LoginRequest" });
 
 //RES Schemas
-export const AuthResponseSchema = z.object({
-    token: z.string().meta({ example: "jwt.token"}),
-    success: z.boolean().meta({ example: "true"}),
-}).meta({ id: "AuthResponse"});
+export const AuthResponseSchema = z
+  .object({
+    token: z.string().meta({ example: "jwt.token" }),
+    success: z.boolean().meta({ example: "true" }),
+  })
+  .meta({ id: "AuthResponse" });
 
-export const LogoutResponseSchema = z.object({
-    success: z.boolean().meta({ example: true }), 
-    message: z.string().meta({ example: "Logged out successfully"})
-}).meta({ id: "LogoutResponse"})
-
-
+export const LogoutResponseSchema = z
+  .object({
+    success: z.boolean().meta({ example: true }),
+    message: z.string().meta({ example: "Logged out successfully" }),
+  })
+  .meta({ id: "LogoutResponse" });
 
 export type LoginInput = z.infer<typeof LoginSchema>;
 export type AuthResponse = z.infer<typeof AuthResponseSchema>;
