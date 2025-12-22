c60be975848f27e549cb82f3e1289b1492991ffe
heikki_vihemaki <heikki.vihemaki@gmail.com>
2025-12-22 14:55:00 +0200

    151 add create endpoint for employment

--- backend/src/openapi/consultants/employments.paths.ts
+++ backend/src/openapi/consultants/employments.paths.ts
@@ -1,4 +1,8 @@
-import { EmploymentListResponseSchema } from "../../schemas/consultants/employment.schema.js";
+import {
+  EmploymentListResponseSchema,
+  EmploymentResponseSchema,
+  EmploymentCreateSchema,
+} from "../../schemas/consultants/employment.schema.js";
 
 export const employmentPaths = {
   "/consultants/{consultantId}/employments": {
@@ -15,7 +19,7 @@ export const employmentPaths = {
       ],
       responses: {
         200: {
-          desciption: "Success",
+          description: "Success",
           content: {
             "application/json": {
               schema: EmploymentListResponseSchema,
@@ -26,6 +30,33 @@ export const employmentPaths = {
         401: { description: "Unauthorized" },
         403: { description: "Forbidden" },
         404: { description: "Consultant not found" },
+        500: { description: "Internal server error" },
+      },
+    },
+  },
+  "/consultants/me/employments": {
+    post: {
+      summary: "Create consultant employment",
+      tags: ["Consultants", "Employments"],
+      requestBody: {
+        required: true,
+        content: {
+          "application/json": {
+            schema: EmploymentCreateSchema, // Unsafe assignmet, for later could consider using zod-to-openapi https://github.com/asteasolutions/zod-to-openapi
+          },
+        },
+      },
+      responses: {
+        201: {
+          description: "Employment created",
+          content: {
+            "application/json": {
+              schema: EmploymentResponseSchema,
+            },
+          },
+        },
+        400: { description: "Invalid input" },
+        401: { description: "Unauthorized" },
         500: { description: "Internal server error" },
       },
     },
