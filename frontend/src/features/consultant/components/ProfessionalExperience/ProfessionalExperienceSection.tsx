0a83c1cc77f643db1543e197aef781e3e6ca4485
Nuutti Rantanen <t66ravovg@mozmail.com>
2026-01-12 11:12:59 +0000

    Resolve "Add adding for work experience for consultant"

--- frontend/src/features/consultant/components/ProfessionalExperience/ProfessionalExperienceSection.tsx
+++ frontend/src/features/consultant/components/ProfessionalExperience/ProfessionalExperienceSection.tsx
@@ -1,14 +1,44 @@
 import { Box, Typography, Stack } from "@mui/material";
 import ProfessionalExperienceItem from "./ProfessionalExperienceItem";
 import type { components } from "@api-types/openapi";
+import AddNewExperience from "./ProfessionalExperienceAdd";
+import { postWorkExperience } from "../../api/consultants.api";
 
 type ConsultantEmploymentList = components["schemas"]["EmploymentListResponse"];
+import { type SkillsResponse } from "../../types/types";
 
 type Props = {
   data: ConsultantEmploymentList;
+  skillData: SkillsResponse;
 };
 
-export default function ProfessionalExperience({ data }: Props) {
+export interface FormedWorkData {
+  companyName: string;
+  description: string;
+  start: string;
+  end: string;
+  visibility: string;
+  jobTitle: string;
+}
+
+export default function ProfessionalExperience({ data, skillData }: Props) {
+  async function AddNewWorkExperience(formData: FormedWorkData) {
+    try {
+      await postWorkExperience({
+        description: formData.description,
+        start: formData.start,
+        end: formData.end,
+        visibility: formData.visibility,
+        projectLinks: [],
+        employer: formData.companyName,
+        jobTitle: formData.jobTitle,
+        skills: ["testing"],
+      });
+    } catch {
+      return;
+    }
+  }
+
   return (
     <Box
       sx={{
@@ -17,6 +47,10 @@ export default function ProfessionalExperience({ data }: Props) {
     >
       <Stack spacing={1} sx={{ maxWidth: 1200 }}>
         <Typography variant="h5">Professional Experience</Typography>
+        <AddNewExperience
+          update={AddNewWorkExperience}
+          skilldata={skillData}
+        ></AddNewExperience>
         <Stack spacing={1}>
           {data.map((item, i) => (
             <ProfessionalExperienceItem key={i} item={item} />
