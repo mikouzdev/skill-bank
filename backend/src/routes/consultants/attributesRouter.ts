e009f3ecfc8de218e2b4258340d290398fd6db14
Philetairos <orkoniitty@hotmail.com>
2026-01-13 16:00:49 +0200

    consultant endpoint work

---
+++ backend/src/routes/consultants/attributesRouter.ts
@@ -0,0 +1,10 @@
+import { Router, type Request, type Response } from "express";
+
+export const attributesRouter = Router();
+
+//TODO: attributes endpoints
+
+//GET /consultants/{consultantId}/attributes
+//POST /consultants/me/attributes
+//PUT /consultants/me/attributes/{attributeId}
+//DELETE /consultants/me/attributes/{attributeId}
--- backend/src/routes/consultants/consultantsRootRouter.ts
+++ backend/src/routes/consultants/consultantsRootRouter.ts
@@ -3,10 +3,14 @@ import { consultantsRouter } from "./consultantsRouter.js";
 import { employmentsRouter } from "./employmentsRouter.js";
 import { projectsRouter } from "./projectsRouter.js";
 import { skillsRouter } from "./skillsRouter.js";
+import { attributesRouter } from "./attributesRouter.js";
+import { pageSectionsRouter } from "./pageSectionsRouter.js";
 
 export const consultantsRootRouter = Router();
 
 consultantsRootRouter.use("/", consultantsRouter);
-consultantsRootRouter.use("/", employmentsRouter);
-consultantsRootRouter.use("/", projectsRouter);
+consultantsRootRouter.use("/employments", employmentsRouter);
+consultantsRootRouter.use("/projects", projectsRouter);
+consultantsRootRouter.use("/attributes", attributesRouter);
 consultantsRootRouter.use("/skills", skillsRouter);
+consultantsRootRouter.use("/sections", pageSectionsRouter);
