079296c513dcc15a8674ebd55ae2334674dc7f84
unknown <anicyna@hotmail.com>
2026-01-15 10:33:45 +0200

    Add view for consultants to list consultants

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -7,6 +7,7 @@ import LoginPage from "../features/Login/LoginPage";
 import ConsultantProfileSettings from "../features/consultant/pages/ConsultantProfileSettings";
 import ConsultantProfilePage from "../features/consultant/pages/ConsultantProfilePage";
 import { ConsultantListView } from "../features/sales/pages/ConsultantListView";
+import { ConsultantListConsultants } from "../features/consultant/pages/ConsultantListConsultants";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         <Route path="/me/edit" element={<ConsultantProfileSettings />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/sales" element={<ConsultantListView />} />
+        <Route path="/listConsultants" element={<ConsultantListConsultants />} />
       </Routes>
     </ThemeProvider>
   );
