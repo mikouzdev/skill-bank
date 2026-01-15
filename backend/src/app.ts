ad09f2cb95c98845ec8664de0117b9de8a1c66c2
Philetairos <orkoniitty@hotmail.com>
2026-01-15 15:43:50 +0200

    login endpoint

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { consultantsRootRouter } from "./routes/consultants/consultantsRootRouter.js";
-import { skillsRouter } from "./routes/consultants/skillsRouter.js";
+import { usersRouter } from "./routes/auth/usersRouter.js";
 
 const app = express();
 app.use(express.json());
@@ -9,5 +9,6 @@ app.use(express.urlencoded({ extended: false }));
 // TODO: move profile pictures to Azure blob storage
 app.use("/static", express.static("uploads/profile_pictures"));
 app.use("/consultants", consultantsRootRouter);
+app.use("/auth", usersRouter);
 
 export default app;
