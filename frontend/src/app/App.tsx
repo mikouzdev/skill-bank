914325c85e700dcee1da864171424200f0140bc3
Miko Kortelainen <mikokortelainen2003@gmail.com>
2026-01-19 10:11:50 +0000

    Resolve "Create a sidebar and layout for it"

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -2,26 +2,32 @@ import { ThemeProvider, CssBaseline } from "@mui/material";
 import { theme } from "../styles/theme";
 import { Routes, Route } from "react-router-dom";
 
-
 import LoginPage from "../features/Login/LoginPage";
 import ConsultantProfileSettings from "../features/consultant/pages/ConsultantProfileSettings";
 import ConsultantProfilePage from "../features/consultant/pages/ConsultantProfilePage";
 import { ConsultantListView } from "../features/sales/pages/ConsultantListView";
 import { ManageUsersPage } from "../features/admin/pages/ManageUsersPage";
 import { ConsultantListConsultants } from "../features/consultant/pages/ConsultantListConsultants";
+import { SidebarLayout } from "./layout/SidebarLayout";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      
+
       <Routes>
-        <Route path="/me" element={<ConsultantProfilePage />} />
-        <Route path="/me/edit" element={<ConsultantProfileSettings />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/sales" element={<ConsultantListView />} />
-        <Route path="/adminManageUsers" element={<ManageUsersPage />} />
-        <Route path="/listConsultants" element={<ConsultantListConsultants />} />
+
+        <Route element={<SidebarLayout />}>
+          <Route path="/me" element={<ConsultantProfilePage />} />
+          <Route path="/me/edit" element={<ConsultantProfileSettings />} />
+          <Route path="/sales" element={<ConsultantListView />} />
+          <Route path="/adminManageUsers" element={<ManageUsersPage />} />
+          <Route
+            path="/listConsultants"
+            element={<ConsultantListConsultants />}
+          />
+        </Route>
       </Routes>
     </ThemeProvider>
   );
