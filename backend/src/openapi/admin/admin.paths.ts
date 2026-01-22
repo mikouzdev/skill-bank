c14a2bed544de99bddb625f8a2e726e3679a5d1a
Philetairos <orkoniitty@hotmail.com>
2026-01-22 15:17:17 +0200

    post admin endpoint for new users

--- backend/src/openapi/admin/admin.paths.ts
+++ backend/src/openapi/admin/admin.paths.ts
@@ -1,5 +1,7 @@
 import {
   AllUsersResponseSchema,
+  FullUserResponseSchema,
+  UserBodySchema
 } from "../../schemas/admin/admin.schema.js";
 
 export const adminPaths = {
@@ -13,6 +15,32 @@ export const adminPaths = {
                     content: {
                         "application/json": { schema: AllUsersResponseSchema },
                     },
+                },
+                "500": {
+                    description: "Server error"
+                },
+            },
+        },
+        post: {
+            summary: "Post new user",
+            tags: ["Admin"],
+            requestBody: {
+                required: true,
+                content: {
+                    "application/json": {
+                    schema: UserBodySchema, // Unsafe assignmet, for later could consider using zod-to-openapi https://github.com/asteasolutions/zod-to-openapi
+                    },
+                },
+            },
+            responses: {
+                "201": {
+                    description: "User created",
+                    content: {
+                        "application/json": { schema: FullUserResponseSchema },
+                    },
+                },
+                "409": {
+                    description: "User email already in use"
                 },
                 "500": {
                     description: "Server error"
