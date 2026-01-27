d1fcbf01760caa36b225d3303043bba9371b954a
Miko Kortelainen <mikokortelainen2003@gmail.com>
2026-01-27 12:36:12 +0000

    Resolve "Add functionality to delete user as admin"

--- frontend/src/features/admin/components/DeleteUsersDialog.tsx
+++ frontend/src/features/admin/components/DeleteUsersDialog.tsx
@@ -9,48 +9,63 @@ import {
   Box,
 } from "@mui/material";
 
-
-type SelectedUser = {id: number, name: string}
+type SelectedUser = { id: number; name: string };
 type Props = {
   selectedUsers: SelectedUser[];
+  onDelete: () => Promise<boolean>;
 };
 
-export function DeleteUsersDialog({selectedUsers} : Props) {
+export function DeleteUsersDialog({ selectedUsers, onDelete }: Props) {
   const [open, setOpen] = useState(false);
-  
+  const [loading, setLoading] = useState<boolean>(false);
+
+  async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    setLoading(true);
+
+    const success = await onDelete();
+    setLoading(false);
+
+    // close form only on success
+    if (success) {
+      setOpen(false);
+    }
+  }
+
   return (
     <>
-      <Button variant="contained" sx={{backgroundColor: "red", "&:hover": { backgroundColor: "darkred" }}}  onClick={() => setOpen(true)}>
+      <Button
+        variant="contained"
+        sx={{
+          backgroundColor: "red",
+          "&:hover": { backgroundColor: "darkred" },
+        }}
+        onClick={() => setOpen(true)}
+      >
         Delete selected users
       </Button>
 
-      <Dialog open={open} onClose={() => setOpen(false)} fullWidth maxWidth="sm">
+      <Dialog
+        open={open}
+        onClose={() => setOpen(false)}
+        fullWidth
+        maxWidth="sm"
+      >
         <DialogTitle>Remove {selectedUsers.length} user(s)</DialogTitle>
 
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            console.log("submit payload:", selectedUsers); //pass to console for debuging
-            
-            setOpen(false);
-          }}
-        >
+        <form onSubmit={(e) => void handleSubmit(e)}>
           <DialogContent>
             <Stack direction="column" spacing={2} sx={{ mt: 1 }}>
-               {selectedUsers.map((u) => (
-              <Box key={u.id}>{u.name}</Box>
-            ))}
-              </Stack>
+              {selectedUsers.map((u) => (
+                <Box key={u.id}>{u.name}</Box>
+              ))}
+            </Stack>
           </DialogContent>
 
           <DialogActions>
             <Button onClick={() => setOpen(false)}>Cancel</Button>
-            <Button type="submit" variant="contained"
-            onClick={() => {
-              console.log("Selected IDs to delete:", selectedUsers);
-              setOpen(false);
-            }}
-            >
+            <Button type="submit" variant="contained" loading={loading}>
               Delete user(s)
             </Button>
           </DialogActions>
