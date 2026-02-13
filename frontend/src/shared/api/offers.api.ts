6dae39519ff59f8054028fefb6a0078bccb261e3
Miko Kortelainen <mikokortelainen2003@gmail.com>
2026-02-13 08:36:13 +0000

    "Add a view where sales can make offers for customers"

--- frontend/src/shared/api/offers.api.ts
+++ frontend/src/shared/api/offers.api.ts
@@ -2,7 +2,13 @@ import type { components } from "@api-types/openapi";
 import { api } from "./api";
 
 type GetOfferPagesResponse = components["schemas"]["GetOfferPagesResponse"];
+type OfferPage = components["schemas"]["OfferPage"];
+type OfferPageBody = components["schemas"]["OfferPageBody"];
 
 export const getOffers = (salesId: number) => {
   return api.get<GetOfferPagesResponse>(`/sales/${salesId}/offers`);
+};
+
+export const createOffer = (salesId: number, payload: OfferPageBody) => {
+  return api.post<OfferPage>(`/sales/${salesId}/offers`, payload);
 };
