4fb7dd842d6f1fd1ebbc9c706c9db67aea07fead
Philetairos <orkoniitty@hotmail.com>
2026-02-25 14:43:19 +0200

    auth/login tests

--- backend/cypress/e2e/api-auth.spec.cy.ts
+++ backend/cypress/e2e/api-auth.spec.cy.ts
@@ -1,15 +1,54 @@
-context("GET /consultants", () => {
-  it("gets a list of consultants", () => {
-    cy.request("GET", "/consultants").then((response) => {
-      expect(response.status).to.eq(200)
-      expect(response.body).length.to.be.greaterThan(1)
-      response.body.forEach((element: any) => {
-        expect(element).to.have.property('id')
-        expect(element).to.have.property('userId')
-        expect(element).to.have.property('description')
-        expect(element).to.have.property('roleTitle')
-        expect(element).to.have.property('profilePictureUrl')
-      });
+context("POST /auth/login", () => {
+  it("logs in and gets token, then logs out", () => {
+    cy.request({
+      method: "POST",
+      url: "/auth/login",
+      body: {
+        "email": "alice@demo.com",
+        "password": "hashed-password"
+      }
+    }).then((response) => {
+      expect(response.status).to.eq(200);
+      expect(response.body.token).to.not.be.null;
+      expect(response.body).to.have.property("success", true);
+      let token = response.body.token;
+      cy.request({
+        method: "POST",
+        url: "/auth/logout",
+        headers: {
+          Authorization: "Bearer " + token,
+        }
+      }).then((response) => {
+        expect(response.status).to.eq(200);
+        expect(response.body.message).to.not.be.null;
+        expect(response.body).to.have.property("success", true);
+      })
+    })
+  })
+  it("logs in with wrong password and does not get token", () => {
+    cy.request({
+      method: "POST",
+      url: "/auth/login",
+      body: {
+        "email": "alice@demo.com",
+        "password": "wrong-password"
+      },
+      failOnStatusCode: false
+    }).then((response) => {
+      expect(response.status).to.eq(401);
+    })
+  })
+  it("logs in with wrong email and does not get token", () => {
+    cy.request({
+      method: "POST",
+      url: "/auth/login",
+      body: {
+        "email": "alisa@demo.com",
+        "password": "hashed-password"
+      },
+      failOnStatusCode: false
+    }).then((response) => {
+      expect(response.status).to.eq(401);
     })
   })
 })
